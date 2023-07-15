c8f596c88229d7290c44447742f608e69ca4e07b
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-07-15 18:18:30 +0400

    6th day

--- 12-static-block/app.java
+++ 12-static-block/app.java
@@ -2,16 +2,18 @@ class Mobile
 {
     String brand ;
     int price ;
-    String type;
+   static String type;
 
     // Static block
 
     static 
     {
-        
+        type = "Phone";
+        System.out.println("This calls only once , when class initially load");
     }
 
-    Mobile()
+    // Constructor
+   public Mobile()
     {
         brand = "Samsung";
         price = 500;
@@ -26,20 +28,38 @@ public class app
 {
     public static void main(String args[])
     {
-        Mobile m1 = new Mobile();
-        Mobile m2 = new Mobile();
-        Mobile m3 = new Mobile();
+        // Mobile m1 = new Mobile();
+        // Mobile m2 = new Mobile();
+        // Mobile m3 = new Mobile();
         
-        Mobile phones[] = new Mobile[3];
+        // m2.type = "Smartphone"; // It changes all objects those are subclasses
+     //   // It works but in static keyword , we must use class name
 
-        phones[0] = m1;
-        phones[1] = m2;
-        phones[2] = m3;
+        // Mobile.type = "Smartphone";
+     //   //If we need to change all the types of the mobile we can use static block
 
-        for(Mobile i : phones)
-        {
-           System.out.println(i.brand + ":" + i.price + ":" + i.type);
-        }
-        
+
+        // Mobile phones[] = new Mobile[3];
+
+        // phones[0] = m1;
+        // phones[1] = m2;
+        // phones[2] = m3;
+
+        // for(Mobile i : phones)
+        // {
+        //    System.out.println(i.brand + ":" + i.price + ":" + i.type);
+        // }
+
+
+      //  /// What if we need to initial load for type but we don't create
+      //  // any obj from this class
+
+    //   ANSWER:
+
+      // Note: in tutorial he says Class.forname , but i don't need       
+    // In Java, when a class with a static block is loaded by the Java Virtual Machine (JVM), the static block is executed regardless of whether you create an object of that class or not. The static block is executed once, during the initialization of the class, and it is used to initialize static variables or perform any other necessary setup for the class.
+    // So, even if you don't create an object of the class, the static block will still be executed, and the values inside the static block will be loaded and initialized. This ensures that the static variables or any other static initialization code is properly set up before they are accessed by other parts of the program.
+        System.out.println(Mobile.type);
+
     }
 }
