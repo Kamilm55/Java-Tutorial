59016b028ee78891ce9cc6ee17e72adccea3bcf5
Kamilm55 <kamilmmmdov2905@gmail.com>
2024-06-30 12:14:38 +0400

    Abstract factory design deep dive

--- DesignPatterns/src/main/java/com/FactoryDesign/App.java
+++ DesignPatterns/src/main/java/com/FactoryDesign/App.java
@@ -131,12 +131,363 @@ class SteakFactory extends FoodFactory {
     }
 }
 
- class App
+
+/////////////////////////////////////////////////Abstract Factory Components \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+interface Button {
+   // void paint(); -> change to ->
+    void paint(String btnType);
+}
+
+class WindowsButton implements Button {
+    public void paint(String btnType) {
+        System.out.println("Rendering a button in Windows style.");
+    }
+}
+
+class MacButton implements Button {
+    public void paint(String btnType) {
+        System.out.println("Rendering a button in Mac style.");
+    }
+}
+
+class LinuxButton implements Button {
+    public void paint(String btnType) {
+        System.out.println("Rendering a button in Linux style.");
+    }
+}
+
+interface Checkbox {
+    void paint();
+}
+
+class WindowsCheckbox implements Checkbox {
+    public void paint() {
+        System.out.println("Rendering a checkbox in Windows style.");
+    }
+}
+
+class MacCheckbox implements Checkbox {
+    private int countOfBoxes;
+    public MacCheckbox(int countOfBoxes) {
+        this.countOfBoxes = countOfBoxes;
+    }
+
+    public void paint() {
+        System.out.println("Rendering a checkbox in Mac style.");
+    }
+}
+
+class LinuxCheckbox implements Checkbox {
+    public void paint() {
+        System.out.println("Rendering a checkbox in Linux style.");
+    }
+}
+
+interface TextField {
+    void paint();
+}
+
+class WindowsTextField implements TextField {
+    public void paint() {
+        System.out.println("Rendering a text field in Windows style.");
+    }
+}
+
+class MacTextField implements TextField {
+    public void paint() {
+        System.out.println("Rendering a text field in Mac style.");
+    }
+}
+
+class LinuxTextField implements TextField {
+    public void paint() {
+        System.out.println("Rendering a text field in Linux style.");
+    }
+}
+
+interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+    TextField createTextField();
+}
+
+class WindowsFactory implements GUIFactory {
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+
+    public TextField createTextField() {
+        return new WindowsTextField();
+    }
+}
+
+class MacFactory implements GUIFactory {
+    public Button createButton() {
+        return new MacButton();
+    }
+
+    public Checkbox createCheckbox() {
+        int countOfBoxes = 5; // default box count -> by encapsulating this creation, I don't need to know specific info about MacCheckBox (default count)
+        return new MacCheckbox(countOfBoxes);
+    }
+
+    public TextField createTextField() {
+        return new MacTextField();
+    }
+}
+
+class LinuxFactory implements GUIFactory {
+    public Button createButton() {
+        return new LinuxButton();
+    }
+
+    public Checkbox createCheckbox() {
+        return new LinuxCheckbox();
+    }
+
+    public TextField createTextField() {
+        return new LinuxTextField();
+    }
+}
+
+class App
 {
+         // Not Ideal code \\
+    private WindowsButton windowsButton; // if we create var with concrete class type, it is not efficient
+    private WindowsCheckbox windowsCheckbox;
+    private WindowsTextField windowsTextField;
+    // Firstly, our app supports only windows, then we need Mac -> and we add Mac (we cannot add with same name, we must rename for mac)
+    private MacButton macButton;
+    private MacCheckbox macCheckbox;
+    private MacTextField macTextField;
+    // Then we need Linux
+    private LinuxButton linuxButton;
+    private LinuxCheckbox linuxCheckbox;
+    private LinuxTextField linuxTextField;
+
+        // Ideal code \\
+    private Button button;
+    private Checkbox checkbox;
+    private TextField textField;
+        //          \\
     public static void main(String[] args) {
         //FactoryDesign1();
-        FactoryDesign2();
+        //FactoryDesign2();
+        AbstractFactoryDesign();
     }
+
+    private static void AbstractFactoryDesign() {
+        // Obsidian canvas deep explanation -> obsidian://open?vault=Obsidian%20Vault&file=Abstract%20Factory%20Design%20Pattern.canvas
+        // YouTube explanation -> https://www.youtube.com/watch?v=v-GiuMmsXj4&t=20s
+        // S,SRP +
+        // O,OCP +
+        // L,LSP -  --> It states that objects of a superclass should be replaceable with objects of a subclass without affecting the functionality of the program. This principle ensures that a subclass can stand in for its superclass without the need for modification
+        // I,ISP -  --> Interface Segregation Principle -> This principle states that no client should be forced to depend on methods it does not use. Instead of one fat interface, multiple small and specific interfaces are preferred. This avoids the implementation of unnecessary methods in a class, leading to more maintainable and modular code
+        // D,DIP +
+        // Encapsulation, Inheritance, Polymorphism, Abstraction, DRY +
+        // Polymorphism --> I have defined interfaces Button, Checkbox, TextField, and GUIFactory. Each concrete class implements one of these interfaces. This is where polymorphism comes into play. For example, WindowsButton, MacButton, and LinuxButton all implement the Button interface. buttons with same type (Button interface) can operate differently from one another
+
+        // todo: Implementing LSP -> add feature that some subclass of interface does not support and throw exception which violates lsp
+        //  --> then refactor with separating more flexible interfaces
+
+        // todo: Implementing ISP -> add some methods to components and implement multiple small interfaces -> only needed interfaces must be implemented
+
+        // QUESTION: We can achieve clean code with changing products type to interface from concrete class, why we need factories?
+
+        App app = new App();
+        String os = System.getProperty("os.name").toLowerCase();
+        app.createUiInIdealCode(os);
+        app.IdealInOtherMethod1(os);
+        app.IdealInOtherMethod2();
+
+
+        app.notIdealCreateUI(os);
+        app.NotIdealInOtherMethod1(os);
+        app.NotIdealInOtherMethod2(os);
+
+        app.paint();
+    }
+
+    private void IdealInOtherMethod2() {
+        // We want paint for current os with extracting method for painting
+
+        // SRP
+        // responsibility 1: Based on currently used os paint components
+        // DIP --> Dependency Inversion Principle --> we don't depend on concrete class types, instead of we depend on interfaces which is more abstract, flexible
+        paintInIdealCode(button,checkbox,textField);// we send components
+    }
+
+    private void paintInIdealCode(Button button, Checkbox checkbox, TextField textField) {
+        button.paint("Draw");
+        checkbox.paint();
+        textField.paint();
+    }
+
+    private void NotIdealInOtherMethod2(String os) {
+        // We want paint for current os with extracting method for painting
+
+        if (os.contains("win")) {
+            windowsButton = new WindowsButton();
+            windowsCheckbox = new WindowsCheckbox();
+            windowsTextField = new WindowsTextField();
+
+            // In ideal code we don't need to interact with client code (main flow)
+            // It also violates SOLID --> SRP -> The more increase features, the more violation of srp increase
+            // responsibility 1: Which os currently use
+            // responsibility 2: Based on currently used os paint components
+            paintInNotIdealCode(windowsButton,windowsCheckbox,windowsTextField);// we send components
+        }
+        else if (os.contains("mac")) {
+            macButton = new MacButton();
+            macCheckbox = new MacCheckbox(12);
+            macTextField = new MacTextField();
+
+            paintInNotIdealCode2(macButton,macCheckbox,macTextField);
+        }
+        else if (os.contains("linux")) {
+            linuxButton = new LinuxButton();
+            linuxCheckbox = new LinuxCheckbox();
+            linuxTextField = new LinuxTextField();
+
+            paintInNotIdealCode3(linuxButton,linuxCheckbox,linuxTextField);
+        } else {
+            throw new UnsupportedOperationException("Unsupported OS");
+        }
+    }
+
+    private void paintInNotIdealCode(WindowsButton button, WindowsCheckbox checkbox, WindowsTextField textField) {
+        button.paint("Draw");
+        checkbox.paint();
+        textField.paint();
+    }
+    private void paintInNotIdealCode2(MacButton button, MacCheckbox checkbox, MacTextField textField) {
+        button.paint("Draw");
+        checkbox.paint();
+        textField.paint();
+    }
+    private void paintInNotIdealCode3(LinuxButton button, LinuxCheckbox checkbox, LinuxTextField textField) {
+        button.paint("Draw");
+        checkbox.paint();
+        textField.paint();
+    }
+
+    //
+    private void IdealInOtherMethod1(String os) {
+        // We want paint for current os
+        button.paint("Draw");
+        checkbox.paint();
+        textField.paint();
+    }
+
+    private void NotIdealInOtherMethod1(String os) {
+        // I want to use button which is currently in use, I don't know specific info which os currently have
+        // for this I must recall  if-else statements --> violation of DRY
+
+        // We want paint for current os
+
+        if (os.contains("win")) {
+            windowsButton = new WindowsButton();
+            windowsCheckbox = new WindowsCheckbox();
+            windowsTextField = new WindowsTextField();
+
+            windowsButton.paint("Draw");
+            windowsCheckbox.paint();
+            windowsTextField.paint();
+        }
+        else if (os.contains("mac")) {
+            macButton = new MacButton();
+            macCheckbox = new MacCheckbox(12);
+            macTextField = new MacTextField();
+
+            macButton.paint("Draw");
+            macCheckbox.paint();
+            macTextField.paint();
+        }
+        else if (os.contains("linux")) {
+            linuxButton = new LinuxButton();
+            linuxCheckbox = new LinuxCheckbox();
+            linuxTextField = new LinuxTextField();
+
+            linuxButton.paint("Draw");
+            linuxCheckbox.paint();
+            linuxTextField.paint();
+        } else {
+            throw new UnsupportedOperationException("Unsupported OS");
+        }
+
+    }
+
+
+    private void notIdealCreateUI(String os) {
+        // SOLID -> 2.OCP --> we cannot extend our code
+
+        // Firstly, our app supports only windows, then we need Mac
+
+        if (os.contains("win")) {
+            windowsButton = new WindowsButton();
+            windowsCheckbox = new WindowsCheckbox();
+            windowsTextField = new WindowsTextField();
+        }
+        // Adding Mac
+        else if (os.contains("mac")) {
+            // If mac components names was same, we could change easily, but we could not
+            // --> we must also change variables names which is violates ocp --> close for modification --> actually we must modify
+            macButton = new MacButton();
+            macCheckbox = new MacCheckbox(12);
+            macTextField = new MacTextField();
+        }
+        // Adding linux
+        else if (os.contains("linux")) {
+            // --> we must also change variables names which is violates ocp --> close for modification --> actually we must modify
+            // They also make difficult to operate something in different places, imagine that we need use these components
+            // across all pages with multiple methods, by defining concrete class type "not defining abstract type (interface)" we violate abstraction
+            // we cannot use easily in other methods
+            linuxButton = new LinuxButton();
+            linuxCheckbox = new LinuxCheckbox();
+            linuxTextField = new LinuxTextField();
+        } else {
+            throw new UnsupportedOperationException("Unsupported OS");
+        }
+    }
+
+    public void createUiInIdealCode(String os) {
+
+        //Learn: There are already have 3 variables with type of interface (Button,Checkbox,TextField)  --> We can change this for requirements, for ex: if our need for Linux
+        // all components can inject for this type (with interface abstraction) --> inside not ideal code we cannot change easily
+
+        // SRP
+        // responsibility 1: Which os currently use
+
+        if (os.contains("win")) {
+            button = new WindowsButton();
+            checkbox = new WindowsCheckbox();
+            textField = new WindowsTextField();
+        } else if (os.contains("mac")) {
+            button = new MacButton();
+            checkbox = new MacCheckbox(12);
+            textField = new MacTextField();
+        }
+        // Adding linux
+        else if (os.contains("linux")) {
+            button = new LinuxButton();
+            checkbox = new LinuxCheckbox();
+            textField = new LinuxTextField();
+        } else {
+            throw new UnsupportedOperationException("Unsupported OS");
+        }
+
+    }
+    public void paint() {
+        button.paint("Submit");
+        checkbox.paint();
+        textField.paint();
+    }
+
+    ///////////////////////////////////////////  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
     private static void FactoryDesign2() {
         // Learn:
